123c438b22ce6239c113476c64666221b9c80d1b
omarrui <rouiguio919@gmail.com>
2025-02-19 14:20:19 +0100

    task 6

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program generates a random number, calculates its last
+ * digit, and prints whether it is greater than 5, is 0, or is less than 6
+ * and not 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return(0);
+}
