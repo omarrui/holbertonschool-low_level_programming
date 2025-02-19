7e5032172361712541fb4facac7d0dbfdfde4aa2
omarrui <rouiguio919@gmail.com>
2025-02-19 10:57:29 +0100

    task 4

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+	int x;
+	i = 97;
+	x=65;
+	while (i <= 122)
+	{
+	putchar(i);
+	i++;
+	}
+	while (x <= 90)
+	{
+	putchar(x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
