77b4b3140b189da43bb0003575085d791fc6a11e
omarrui <rouiguio919@gmail.com>
2025-02-19 11:01:57 +0100

    task 4

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program generates a random number, calculates its last
+ * digit, and prints whether it is greater than 5, is 0, or is less than 6
+ * and not 0.
+ *
+ * Return: Always 0 (Success)
+ */
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 int main(void)
 {
