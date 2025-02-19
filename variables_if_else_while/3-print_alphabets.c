48707555ed84f6a31fc518c56274ae652561b64c
omarrui <rouiguio919@gmail.com>
2025-02-19 10:59:26 +0100

    task 4

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,19 @@ int main(void)
 {
 	int i;
 	int x;
+
 	i = 97;
 	x=65;
+
 	while (i <= 122)
 	{
-	putchar(i);
-	i++;
+		putchar(i);
+		i++;
 	}
 	while (x <= 90)
 	{
-	putchar(x);
-	x++;
+		putchar(x);
+		x++;
 	}
 	putchar('\n');
 	return (0);
