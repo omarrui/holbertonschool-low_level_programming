c72a086807a8033dfbf9e7e890db805526e4e5ac
omarrui <rouiguio919@gmail.com>
2025-02-20 20:26:47 +0100

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Adds two integers and prints the result
+ * @x: The first integer
+ * @y: The second integer
+ *
+ * Return: The sum of x and y
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
