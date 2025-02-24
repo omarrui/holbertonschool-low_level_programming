771b4ec4cfeeaf7f1d3d5819c6dbaa4f0227b349
omarrui <rouiguio919@gmail.com>
2025-02-24 15:23:53 +0100

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
