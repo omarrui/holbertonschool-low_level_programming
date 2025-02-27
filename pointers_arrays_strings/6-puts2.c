ff72ac443dda4d574137498c7e00f303b9fb6bb4
omarrui <rouiguio919@gmail.com>
2025-02-27 20:30:50 +0100

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,13 @@
  */
 void puts2(char *str)
 {
-	int i;
+    int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (i % 2 == 0)
 		_putchar(str[i]);
 	}
 
 	_putchar('\n');
 }
-
