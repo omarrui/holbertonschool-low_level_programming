96927c67d672b45afed37b650fb7fe15d6f0057f
omarrui <rouiguio919@gmail.com>
2025-03-01 23:20:13 +0100

    task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *leet - encodes a string into 1337
+ *@str : the input string to modify
+ *
+ *Return:pointer to the modified string
+ */
+char * leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str [i] == letters[j])
+			{
+				str[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
