445c083c228eb1ac2568e5c82225119a31bbf063
omarrui <rouiguio919@gmail.com>
2025-03-05 16:17:09 +0100

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,20 +1,15 @@
 #include "main.h"
 /**
- *strlen_recursion: will count how many ch are in the string
+ *_strlen_recursion: will count how many ch are in the string
  *
  *@s: is the string we gonna be counting
  *
- *return: int
+ *return: lrn of yhe str
 */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		return 0;
-	}
-	else
-	{
-		return 1 + _strlen_recursion(s + 1);
-	}
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
