092ea112828b2e0ca70e421e43f0b6101ece9eb9
omarrui <rouiguio919@gmail.com>
2025-03-05 21:21:25 +0100

    task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *factorial: this function calculates the factorail of a number
+ *
+ *@n: base number
+ *
+ *return: the factorail of n
+*/
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return(-1);
+	return(n * factorial(n-1));
+}
