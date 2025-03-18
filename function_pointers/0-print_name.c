05b5a79fc207b79c8bd0cac63fd4e390efcc03d3
omarrui <rouiguio919@gmail.com>
2025-03-19 00:59:18 +0100

    fix task 1&0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,16 @@
 #include "function_pointers.h"
 #include <stddef.h>
+
 /**
- * print_name - prints names
- * @f: void function
- * 
- * Return: void
- */
-void print_name(char *name, void(*f)(char *))
+* print_name - prints a name.
+*
+* @name: Name of the person.
+* @f: Void function.
+*
+* Return: Always void.
+*/
+void print_name(char *name, void (*f)(char *))
 {
-   if (name == NULL)
-        return;
-    if (f != NULL)
-        f(name);
-    else
-        return;
+	if (f != NULL && name != NULL)
+		f(name);
 }
--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,19 +2,19 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function on each element of an array
- * @array: the array in process
- * @size: the number of elements in the array
- * @action: pointers to the function to be applied to each element
- * 
- * Return: void
- */
+* array_iterator - executes a function on each element of an array
+* @array: the array in process
+* @size: the number of elements in the array
+* @action: pointers to the function to be applied to each element
+*
+* Return: void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if(array == NULL || action == NULL)
-        return;
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
