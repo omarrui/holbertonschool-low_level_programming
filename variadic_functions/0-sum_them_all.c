1695293e7bf3ee23321e15f0ac814145c370b345
omarrui <rouiguio919@gmail.com>
2025-03-20 23:20:18 +0100

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,11 +15,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+
 	if (n == 0)
 		return (0);
 	while (i < n)
 	{
-		sum = va_arg(args, int);
+		sum += va_arg(args, int);
 		i++;
 	}
 	va_end(args);
