3aa6151c581a953b70b8423b050f11a230cc11b2
omarrui <rouiguio919@gmail.com>
2025-03-20 17:55:04 +0100

    task 3 - fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,40 +1,35 @@
+#include "3-calc.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 
 /**
-* main - Performs simple operations
+* main - check the code
 *
-* @argc: Argument count
-* @argv: Argument vector
+* @argc: Number of parameters
+* @argv: Array of elements
 *
-* Return: 0 on success, error codes otherwise
+* Return: Always 0.
 */
 int main(int argc, char *argv[])
 {
-	int num2, num1, result;
+	int num1, num2;
 
-	int (*operation)(int, int);
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
-		printf("error⑊n");
+		printf("Error\n");
 		return (98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	result = operation(num1, num2);
-	printf("%d\n", result);
-
+	printf("%d\n", op_func(num1, num2));
 	return (0);
-
 }
