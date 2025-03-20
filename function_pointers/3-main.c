37b0ba6c1c89d2e2abcf158c8583683a333680ee
omarrui <rouiguio919@gmail.com>
2025-03-20 17:41:00 +0100

    task 3-fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -32,6 +32,12 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	result = operation(num1, num2);
 	printf("%d\n", result);
 
