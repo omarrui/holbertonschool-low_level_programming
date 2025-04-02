fc143712084e9300c276e27c8d59689ac4adf719
omarrui <rouiguio919@gmail.com>
2025-04-02 16:04:40 +0200

    creating the putchar function also  a non empty README page and the setting up the lists file

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_h
+#include <stddef.h>
+/**
+ * struct lists_s -singly linked list
+ * @str: string 
+ * @len: length of the stirng
+ * @next: points to the next node
+ * 
+ * Description: single linked list node structure 
+ */
+typedef struct lists_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} lists_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
