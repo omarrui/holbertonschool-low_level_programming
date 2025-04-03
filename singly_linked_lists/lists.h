0401be4421ac5f4dfe61cd5277259b0d395e4aa0
omarrui <rouiguio919@gmail.com>
2025-04-03 14:21:44 +0200

    fixing lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,19 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
+
 /**
- * struct lists_s -singly linked list
- * @str: string 
- * @len: length of the stirng
+ * struct list_s - singly linked list
+ * @str: string (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
- * 
- * Description: single linked list node structure 
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -23,4 +25,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
