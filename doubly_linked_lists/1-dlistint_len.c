c08ec5bb447cf35ab2d134b5397d81de490e0ad7
omarrui <rouiguio919@gmail.com>
2025-04-04 15:19:01 +0200

    fixing betty errors

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * dlistint_len - count the number of elements we got in the list
- * 
- * @h: the lsit we r using
- * 
- * Return: the number of elements
- */
+* dlistint_len - that returns the number of elements in a linked list
+*
+* @h: the list to use
+*
+* Return: the number of elements in a linked list
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-    unsigned int nbE = 0;
+	unsigned int nbE = 0;
 
-    while (h)
-    {
-        nbE++;
-        h = h->next;
-    }
-    return (nbE);
+	while (h != NULL)
+	{
+		nbE++;
+		h = h->next;
+	}
+	return (nbE);
 }
